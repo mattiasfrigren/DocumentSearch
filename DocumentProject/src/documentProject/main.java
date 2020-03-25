bf81b696dee19988d9355a08b0c74d63dd41c188
91matfri <mattiasfrigren@hotmail.com>
2020-03-25 12:20:21 +0100

    version 0.2 added packages and set up first @Test

--- DocumentProject/src/documentProject/main.java
+++ DocumentProject/src/documentProject/main.java
@@ -1,6 +1,9 @@
+package documentProject;
+
 public class main {
 
     public static void main(String[]args){
+
         System.out.println("hello world");
         System.out.println("testing");
     }
