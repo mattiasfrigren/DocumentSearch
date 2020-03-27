d03fa66d27eeecc4a818ea8aef83872a6c22963a
Anara23 <anarikrulit@mail.ru>
2020-03-27 20:46:14 +0100

    just 1 exception in Hub added at the moment
    # Conflicts:
    #	DocumentProject/src/documentProject/Hub.java
    #	out/production/DocumentProject/TestPackage/DocumentLibraryTest.class
    #	out/production/DocumentProject/documentProject/main.class

--- DocumentProject/src/documentProject/Hub.java
+++ DocumentProject/src/documentProject/Hub.java
@@ -1,6 +1,8 @@
 package documentProject;
 
+
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Hub {
@@ -12,12 +14,20 @@ public class Hub {
         while (input !=7) {
             Scanner sc = new Scanner(System.in);
             printMenu();
+            try{
             input = sc.nextInt();
+            }catch (InputMismatchException exception){
+                System.out.println("That's not a number. Please try again.");
+                continue;
+            }
+
             switch (input) {
                 case 1:
+
                     try { library.CreateNewTxtFile(); }
                     catch (IOException e) { e.printStackTrace(); }
                     break;
+
                 case 2:
                     break;
                 case 3:
@@ -37,8 +47,24 @@ public class Hub {
         }
     }
     private void printMenu() {
-        System.out.println("[1] Read .txt from computer and store it.\n[2] Print a document.\n[3] Sort words in a specific document." +
-                "\n[4] Sort words in all documents.\n[5] Search in a specific document.\n[6] Search in all documents.\n[7] Exit system.");
+/*
+        System.out.println("[1] Read .txt from computer and store it.");
+        System.out.println("[2] Print a document.");
+        System.out.println("[3] Sort words in a specific document.");
+        System.out.println("[4] Sort words in all documents.");
+        System.out.println("[5] Search in a specific document.");
+        System.out.println("[6] Search in all documents.");
+        System.out.println("[7] Exit system.");
+*/
+
+        System.out.println(
+                "[1] Read .txt from computer and store it.\n" +
+                "[2] Print a document.\n" +
+                "[3] Sort words in a specific document.\n" +
+                "[4] Sort words in all documents.\n" +
+                "[5] Search in a specific document.\n" +
+                "[6] Search in all documents.\n" +
+                "[7] Exit system.");
     }
     public static Hub getHub() {
         return hub;
