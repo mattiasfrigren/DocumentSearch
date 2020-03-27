03fab77cfee743443faca66b1e25826546b79166
91matfri <mattiasfrigren@hotmail.com>
2020-03-27 15:10:58 +0100

    verions 0.5 Library class is almost done and the test are almost done

--- DocumentProject/src/TestPackage/DocumentLibraryTest.java
+++ DocumentProject/src/TestPackage/DocumentLibraryTest.java
@@ -57,6 +57,7 @@ class DocumentLibraryTest {
         testFile.createNewFile();
         testLibrary.deleteTxtFile(testFile);
         assertFalse(testFile.exists());
+        
     }
     @Test
     void deleteDocumentList(){
