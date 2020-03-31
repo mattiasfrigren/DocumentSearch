5bcbcb5d68aa13b20fc03cd36150903a2daf78bf
HenrikJava <henriklarssonmail@gmail.com>
2020-03-31 16:16:05 +0200

    Completed testcutString and cutString and edited my path

--- DocumentProject/src/documentProject/DifferentLocalStoragePaths.java
+++ DocumentProject/src/documentProject/DifferentLocalStoragePaths.java
@@ -8,7 +8,7 @@ package documentProject;
 //TODO fixed the class to safer paths.
 public class DifferentLocalStoragePaths {
     private static String mattiasPath = "C:\\Users\\91matfri\\IdeaProjects\\documentgroup";
-    private static String henriksPath = "C:\\Users\\78henlar\\desktop\\javaprojekt\\documentgroupny";
+    private static String henriksPath = "C:\\Users\\78henlar\\desktop\\javaprojekt\\documentgroup";
     private static String anarasPath = "C:\\Users\\88anazhu\\IdeaProjects\\documentgroup";
 
     public static String docPath =  mattiasPath;
